ec3eaf01f23113b4015f92124b6fb9e69ea041b6
dk <denis.kolokolov@icloud.com>
2022-11-28 23:26:59 +0100

    Remove .DS_Store from everywhere and addBlog function

---
+++ computed-properties-exercise/script.js
@@ -0,0 +1,77 @@
+const App = {
+  data() {
+    return {
+      selected: '',
+      posts: [
+      {
+        author: '@vFitzgerald',
+        title: 'Quod Ducimus Omnis',
+        label: 'science' },
+
+      {
+        author: '@mPalmer',
+        title: 'Vero Eius Laboriosam Ex Repudiandae',
+        label: 'math' },
+
+      {
+        author: '@mDean',
+        title: 'Magnam Odit',
+        label: 'science' },
+
+      {
+        author: '@tCole',
+        title: 'Velit Mollitia Voluptates Assumenda',
+        label: 'science' },
+
+      {
+        author: '@jCooper',
+        title: 'Eum Commodi Cupiditate',
+        label: 'poetry' },
+
+      {
+        author: '@lLamb',
+        title: 'Amet',
+        label: 'history' },
+
+      {
+        author: '@fMartin',
+        title: 'Voluptatem Fuga Cum Asperiores Error',
+        label: 'science' },
+
+      {
+        author: '@eHayes',
+        title: 'Ipsa Consectetur Rerum Repellat Quia',
+        label: 'math' },
+
+      {
+        author: '@cCollier',
+        title: 'Dolor Nihil Delectus',
+        label: 'science' },
+
+      {
+        author: '@cBenson',
+        title: 'Labore Ipsum Ad Pariatur',
+        label: 'poetry' }],
+
+
+      newTitle: '',
+      newAuthor: '',
+      newLabel: '' };
+
+  },
+  methods: {
+    addBlog() {
+      this.posts.push({
+        title: this.newTitle,
+        author: this.newAuthor,
+        label: this.newLabel
+      })
+      this.newTitle = ''
+      this.newAuthor = ''
+      this.newLabel = ''
+    }
+  }
+};
+
+
+Vue.createApp(App).mount('#app');
